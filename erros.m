97e12442ae0c77800266358d6afc4773fb466c44
cvhloe <141273404+cvhloe@users.noreply.github.com>
2023-11-28 15:17:39 -0300

    Add files via upload

---
+++ erros.m
@@ -0,0 +1,197 @@
+clear; n = 11; H = gerarMatrizHilbert(n); b=ones(n,1);
+
+x0 = zeros(n,1); omega1 = 0.01; omega2 = 0.02; tolerance = 1e-7; max_iterations = 10;
+X = linsolve(H, b);
+
+%% LU
+[L, U] = lu(H);
+
+y = L \ b; x1 = U \ y;
+
+%% Cholesky
+L = cholesky(H);
+
+y = L \ b; x2 = L' \ y;
+
+%% JOR
+[x3, iter] = jor(H, b, x0, omega1, tolerance, max_iterations);
+
+%% SOR
+[x4, iter] = sor(H, b, x0, omega2, tolerance, max_iterations);
+
+%% Max descida
+[x5, iter] = gradiente(H, b, x0, tolerance, max_iterations);
+
+%% Gradientes Conjugados
+[x6, iter] = gradconj(H, b, x0, tolerance, max_iterations);
+
+%% Erros
+Ea_LU = abs(X - x1); Ea_Cholesky = abs(X - x2); Ea_JOR = abs(X - x3); Ea_SOR = abs(X - x4);Ea_MaxDescida = abs(X - x5); Ea_GradConj = abs(X - x6);
+
+
+Er_LU = abs(Ea_LU ./ abs(X)); Er_Cholesky = abs(Ea_Cholesky ./ abs(X)); Er_JOR = abs(Ea_JOR ./ abs(X)); Er_SOR = abs(Ea_SOR ./ abs(X)); Er_MaxDescida = abs(Ea_MaxDescida ./ abs(X)); Er_GradConj = abs(Ea_GradConj ./ abs(X));
+
+
+
+% LU
+figure;
+plot(Er_LU);
+title('Decomposição LU');
+xlabel('n');
+ylabel('Erro');
+
+% Cholesky
+figure;
+plot(Er_Cholesky);
+title('Decomposição Cholesky');
+xlabel('n');
+ylabel('Erro');
+
+% JOR
+figure;
+plot(Er_JOR);
+title('JOR');
+xlabel('n');
+ylabel('Erro');
+
+% SOR
+figure;
+plot(Er_SOR);
+title('SOR');
+xlabel('n');
+ylabel('Erro');
+
+% Max Descida
+figure;
+plot(Er_MaxDescida);
+title('Máxima Descida');
+xlabel('n');
+ylabel('Erro');
+
+% Gradientes Conjugados
+figure;
+plot(Er_GradConj);
+title('Gradientes Conjugados');
+xlabel('n');
+ylabel('Erro');
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% LU
+figure;
+plot(Ea_LU);
+title('Decomposição LU');
+xlabel('n');
+ylabel('Erro');
+
+% Cholesky
+figure;
+plot(Ea_Cholesky);
+title('Decomposição Cholesky');
+xlabel('n');
+ylabel('Erro');
+
+% JOR
+figure;
+plot(Ea_JOR);
+title('JOR');
+xlabel('n');
+ylabel('Erro');
+
+% SOR
+figure;
+plot(Ea_SOR);
+title('SOR');
+xlabel('n');
+ylabel('Erro');
+
+% Max Descida
+figure;
+plot(Ea_MaxDescida);
+title('Máxima Descida');
+xlabel('n');
+ylabel('Erro');
+
+% Gradientes Conjugados
+figure;
+plot(Ea_GradConj);
+title('Gradientes Conjugados');
+xlabel('n');
+ylabel('Erro');
+
+
+% Comparação dos resultados
+figure;
+plot(x1, 'DisplayName', 'LU');
+hold on;
+plot(x2, 'DisplayName', 'Cholesky');
+title('Resultados dos métodos LU e Cholesky');
+xlabel('n');
+ylabel('Resultado');
+legend;
+hold off;
+
+% Comparação dos resultados
+figure;
+plot(X, 'DisplayName', 'Exato');
+hold on;
+plot(x1, 'DisplayName', 'LU');
+title('Resultados exatos VS LU');
+xlabel('n');
+ylabel('Resultado');
+legend;
+hold off;
+
+% Comparação dos resultados
+figure;
+plot(X, 'DisplayName', 'Exato');
+hold on;
+plot(x2, 'DisplayName', 'Cholesky');
+title('Resultados exatos VS Cholesky');
+xlabel('n');
+ylabel('Resultado');
+legend;
+hold off;
+
+% Comparação dos resultados
+figure;
+plot(X, 'DisplayName', 'Exato');
+hold on;
+plot(x3, 'DisplayName', 'JOR');
+title('Resultados exatos VS JOR');
+xlabel('n');
+ylabel('Resultado');
+legend;
+hold off;
+
+% Comparação dos resultados
+figure;
+plot(X, 'DisplayName', 'Exato');
+hold on;
+plot(x4, 'DisplayName', 'SOR');
+title('Resultados exatos VS SOR');
+xlabel('n');
+ylabel('Resultado');
+legend;
+hold off;
+
+% Comparação dos resultados
+figure;
+plot(X, 'DisplayName', 'Exato');
+hold on;
+plot(x5, 'DisplayName', 'Máx Descida');
+title('Resultados exatos VS Máx Descida');
+xlabel('n');
+ylabel('Resultado');
+legend;
+hold off;
+
+% Comparação dos resultados
+figure;
+plot(X, 'DisplayName', 'Exato');
+hold on;
+plot(x6, 'DisplayName', 'Grad Conj');
+title('Resultados exatos VS Grad Conj');
+xlabel('n');
+ylabel('Resultado');
+legend;
+hold off;
+
--- implementacao.m
+++ implementacao.m
@@ -7,10 +7,7 @@ x0 = zeros(n,1);
 omega1 = 0.02 ;
 omega2 = 1.25;
 tolerance = 1e-7;
-max_iterations = 100;
-
-X = linsolve(H, b);
-
+max_iterations = 10;
 %% LU
 
 [L, U] = lu(H);
@@ -56,24 +53,9 @@ Bs = Bs(H);
 srS = spec_rad(Bs)
 interv = 2/srS;
 
-Ea_LU = abs(X - x1);
-Ea_Cholesky = abs(X - x2);
-Ea_JOR = abs(X - x3);
-Ea_SOR = abs(X - x4);
-Ea_MaxDescida = abs(X - x5);
-Ea_GradConj = abs(X - x6);
-
-
-Er_LU = abs(Ea_LU ./ abs(X));
-Er_Cholesky = abs(Ea_Cholesky ./ abs(X));
-Er_JOR = abs(Ea_JOR ./ abs(X));
-Er_SOR = abs(Ea_SOR ./ abs(X));
-Er_MaxDescida = abs(Ea_MaxDescida ./ abs(X));
-Er_GradConj = abs(Ea_GradConj ./ abs(X));
-
 
 graphics_toolkit('gnuplot');
-omega_1 = 0:0.01:interv;
+omega_1 = 0.01:0.01:interv;
 radios_espectrais_JOR = zeros(size(omega_1));
 
 for i = 1:length(omega_1)
@@ -88,7 +70,7 @@ ylabel('Raio Espectral');
 legend('JOR');
 grid on;
 
-omega_2 = 0:0.01:2;
+omega_2 = 0.01:0.01:1.99;
 radios_espectrais_SOR = zeros(size(omega_2));
 
 for i = 1:length(omega_2)
@@ -103,47 +85,4 @@ ylabel('Raio Espectral');
 legend('SOR');
 grid on;
 
-% Gráfico de Convergência
-figure;
-plot(1:length(Ea_JOR), Ea_JOR, 'o-', 1:length(Ea_SOR), Ea_SOR, 's-', 1:length(Ea_MaxDescida), Ea_MaxDescida, '^-', 1:length(Ea_GradConj), Ea_GradConj, 'd-');
-legend('JOR', 'SOR', 'Max Descida', 'Gradientes Conjugados');
-xlabel('Número de Iterações');
-ylabel('Erro Absoluto');
-title('Gráfico de Convergência');
-grid on;
-
-% Gráfico de Dispersão de Resíduos
-figure;
-scatter(1:length(Ea_JOR), Ea_JOR, 'o', 'filled');
-hold on;
-scatter(1:length(Ea_SOR), Ea_SOR, 's', 'filled');
-scatter(1:length(Ea_MaxDescida), Ea_MaxDescida, '^', 'filled');
-scatter(1:length(Ea_GradConj), Ea_GradConj, 'd', 'filled');
-legend('JOR', 'SOR', 'Max Descida', 'Gradientes Conjugados');
-xlabel('Iteração');
-ylabel('Resíduos');
-title('Gráfico de Dispersão de Resíduos');
-grid on;
-
-% Histograma de Erros
-figure;
-hist([Ea_JOR, Ea_SOR, Ea_MaxDescida, Ea_GradConj], 20);
-xlabel('Erro Absoluto');
-ylabel('Frequência');
-title('Histograma de Erros');
-legend('JOR', 'SOR', 'Max Descida', 'Gradientes Conjugados');
-grid on;
-
-% Erro Absoluto vs Erro Relativo
-figure;
-scatter(Er_JOR, Ea_JOR, 'o', 'filled');
-hold on;
-scatter(Er_SOR, Ea_SOR, 's', 'filled');
-scatter(Er_MaxDescida, Ea_MaxDescida, '^', 'filled');
-scatter(Er_GradConj, Ea_GradConj, 'd', 'filled');
-legend('JOR', 'SOR', 'Max Descida', 'Gradientes Conjugados');
-xlabel('Erro Relativo');
-ylabel('Erro Absoluto');
-title('Erro Absoluto vs Erro Relativo');
-grid on;
 
